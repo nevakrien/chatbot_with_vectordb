f88d0c95a618be60d29ae037c2fba9e41dd6b9f0
User <nevo.krien@gmail.com>
2023-10-31 11:12:18 +0200

    better prompting (cant do mircles tho)

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from transformers import  AutoTokenizer,AutoModelForCausalLM
 from optimum.intel import OVModelForCausalLM
 from emb import VectorDB
+from collections import deque
 
 # import os
 # token = os.environ.get('hf_token')
 
-model_name = "HuggingFaceH4/zephyr-7b-beta"#"gpt2"
-identety_prompt = "be cheary and help people try and talk about python"
+#model_name = "/home/user/.cache/huggingface/hub/models--meta-llama--Llama-2-7b-chat-hf/snapshots/94b07a6e30c3292b8265ed32ffdeccfdadf434a8"
+model_name="microsoft/DialoGPT-medium"#"HuggingFaceH4/zephyr-7b-beta"#"gpt2"
+identety_prompt = "IMPORTANT: your name is james you tell jokes!!!"
+CONTEXT=3
 
 def get_next_text(model,tokenizer,input_messages):
 	inputs=tokenizer.apply_chat_template(input_messages,return_tensors='pt',add_generation_prompt=True)
@@ -24,13 +27,15 @@ def openai_format(text,role='system'):
 #assistant
 #user
 
-def make_input(user_db,bot_db,prompt):
+def make_input(user_db,bot_db,history: list ):
+	prompt=history[-1]
 	messages=(
-		[openai_format(identety_prompt),openai_format('user said in the past:')]
-		+[openai_format(x,'user') for x in user_db.search([prompt],3,add=True)[0]]
-		+[openai_format('I the bot said in the past')]
-		+[openai_format(x,'assistant') for x in bot_db.search([prompt],3,add=False)[0]]
-		+[openai_format('user is saying:'),openai_format(prompt,'user')]
+		[openai_format(identety_prompt),openai_format('memories of what the user said in the past:')]
+		+[openai_format(x,'user') for x in user_db.search([prompt],3,add=True)[0] if not x in history]
+		+[openai_format('memories of what I the bot said in the past:')]
+		+[openai_format(x,'assistant') for x in bot_db.search([prompt],3,add=False)[0] if not x in history]
+		+[openai_format('curent conversation:')]
+		+[openai_format(x,'user' if i%2==(CONTEXT-1)%2 else 'assistant') for i,x in enumerate(history)]
 		)
 
 	return messages
@@ -41,13 +46,15 @@ if __name__=='__main__':
 	
 	user_db=VectorDB()
 	bot_db=VectorDB()
+	history=deque(maxlen=CONTEXT)
 
 	next_output=['input text']
 	while(True):
 		prompt=input(next_output[0]+'\n')
 		print(2*("\n"+10*"!"))
 		
-		input_messages = make_input(user_db,bot_db,prompt)
+		history.append(prompt)
+		input_messages = make_input(user_db,bot_db,history)
 		#print(input_messages)
 		#print("\n")
 
--- premission.py
+++ premission.py
@@ -3,13 +3,13 @@ from huggingface_hub import HfApi
 import os
 token = os.environ.get('hf_token')
 
-model_name = "Llama-2-7b-chat-hf"#"https://huggingface.co/meta-llama/Llama-2-7b-chat-hf"
+#model_name = "Llama-2-7b-chat-hf"#"https://huggingface.co/meta-llama/Llama-2-7b-chat-hf"
+model_name = "/home/user/.cache/huggingface/hub/models--meta-llama--Llama-2-7b-chat-hf/snapshots/94b07a6e30c3292b8265ed32ffdeccfdadf434a8"
 
+# hf_api = HfApi()
+# user = hf_api.whoami(token)
+# print(user)
 
-hf_api = HfApi()
-user = hf_api.whoami(token)
-print(user)
-
-tokenizer = AutoTokenizer.from_pretrained(model_name, token=token)
-model = AutoModelForCausalLM.from_pretrained(model_name, token=token)
+tokenizer = AutoTokenizer.from_pretrained(model_name)#, token=token)
+model = AutoModelForCausalLM.from_pretrained(model_name)#, token=token)
 
